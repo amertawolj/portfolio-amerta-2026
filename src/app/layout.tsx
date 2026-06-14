4c8319c5fc80a373cd16bf03ee8d11b37714b744
amertawolj <waodeamerta1@gmail.com>
2026-06-14 20:16:08 +0700

    docs: change metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
-  title: "Amerta's Portfolio",
-  description: "UI/UX & Graphic Design Portfolio",
+  title: "Amerta",
+  description: "Amerta's Personal Space",
 };
 
 export default function RootLayout({
